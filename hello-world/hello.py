aebd4a6b1a393195bde64b6ac6a455ffe61a75b7
skyzismo <lucas.petigato@hotmail.com>
2022-02-28 13:57:51 -0400

    primeiro commit

---
+++ hello-world/hello.py
@@ -0,0 +1,27 @@
+# print('Hello, world!')
+
+# x = 1
+
+# print(type(x))
+
+# x = 1.0
+
+# print(type(x))
+
+# x = True
+
+# print(type(x))
+
+x = 'This is a string'
+
+y = ' and this is another string'
+
+# print(x)
+# print(type(x))
+
+print(x + y)
+
+a = 2
+b = 4
+
+print(a * b)
